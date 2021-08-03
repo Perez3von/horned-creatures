6dff6d88e12710fb536a35ee86be4b974ca0694c
Evon Perez <perez.evon@gmail.com>
2021-08-03 16:47:03 -0400

    added skeleton of app

---
+++ src/ImageItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class ImageItem extends Component {
+  
+    render() { 
+        return ( 
+            <p>
+                IMAGE
+
+            </p>
+         );
+    }
+}
+ 
+export default ImageItem;
---
+++ src/ImageList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class ImageList extends Component {
+    
+    render() { 
+        return ( 
+            <select>
+                <option value='red'>RED</option>
+                <option valur='blue'>Blue</option>
+                <option value='green'>Green</option>
+            </select>
+
+
+
+         );
+    }
+}
+ 
+export default ImageList;
